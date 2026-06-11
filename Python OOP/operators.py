fda08ede50885a49440d76b1ef7f4c77351f7856
Muhammad Hasnain Maqsood <hasnainchohane@gmail.com>
2026-06-11 09:23:05 +0500

    Add files via upload

---
+++ Python OOP/operators.py
@@ -0,0 +1,59 @@
+#Operators
+
+val1=40
+val2=20
+#additiion
+print(val1+val2)
+#subtraction
+print(val1-val2) 
+#multiplication
+print(val1*val2) 
+#Division
+print(val1/val2) 
+#modulus
+print(val1%val2)
+#exponentiation
+print(val1**val2)
+# Second way to do
+sum=val1+val2
+print("sum=",sum)
+#subtraction
+difference=val1-val2
+print("difference=",difference)
+#multiplication
+product=val1*val2
+print("product=",product)
+#Division
+quotient=val1/val2
+print("quotient=",quotient)
+#modulus
+modulus=val1%val2
+print("modulus=",modulus)
+#floor division
+floor_division=val1//val2
+print("floor division=",floor_division)
+#exponentiation
+exponentiation= val1**val2       
+print("exponentiation=",exponentiation)
+#comparison operators
+a=4
+b=5
+#equal to
+print(a==b) 
+#not equal to
+print(a!=b) 
+#greater than
+print(a>b)
+#less than
+print(a<b)
+#greater than or equal to
+print(a>=b) 
+#less than or equal to
+print(a<=b)
+#SECOND WAY TO DO   
+print("a is equal to b:",a==b)
+print("a is not equal to b:",a!=b)
+print("a is greater than b:",a>b)
+print("a is less than b:",a<b)
+print("a is greater than or equal to b:",a>=b)
+print("a is less than or equal to b:",a<=b)
