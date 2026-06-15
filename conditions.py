2fbc5918287094eb0fe54eea95bba307169d7d19
Muhammad Hasnain Maqsood <hasnainchohane@gmail.com>
2026-06-15 16:45:34 +0500

    Add files via upload

---
+++ conditions.py
@@ -0,0 +1,11 @@
+marks=int(input("Enter your marks : "))
+if(marks >=80) :
+  print("Grade is A")
+elif(marks >=70):
+   print("Grade is B")
+elif(marks >=60):
+   print("Grade is C")
+elif(marks >=50):
+   print("Grade is D")
+else:
+    print("You are Fail")         
