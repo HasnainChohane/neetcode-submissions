2f1cb0d9224ff2f5e1055e189528a883de734c9f
Muhammad Hasnain Maqsood <hasnainchohane@gmail.com>
2026-06-16 16:41:32 +0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,17 @@
+# while loop
+val =1
+print("loop start")
+while (val <=5):
+   print ("Hello world")
+   val +=1
+
+print("loop end")
+
+
+# For Loop 
+print("==========For Loop==========")
+for fibo  in range(5):
+   print ("Hi how are you")
+
+
+print ("loop ended")
