200c83841ec19462d699b295b716779745edc66a
Muhammad Hasnain Maqsood <216601502+HasnainChohane@users.noreply.github.com>
2026-06-26 18:44:39 +0500

    Add files via upload

--- course_enroll.py
+++ course_enroll.py
@@ -2,9 +2,9 @@ python_students = {"ali", "sara", "hamza"}
 web_students = {"hamza", "noor", "fatima"}
 
 # Perform set operations and sort the results for predictable output
-both_courses = sorted(python_students.intersection(web_students))
-all_students = sorted(python_students.union(web_students))
-python_only = sorted(python_students.difference(web_students))
+both_courses = (set(sorted(python_students.intersection(web_students))))
+all_students =(set( sorted(python_students.union(web_students))))
+python_only = (set(sorted(python_students.difference(web_students))))
 
 # Print the clean, sorted lists
 print(f"Both courses: {both_courses}")
---
+++ functions.py
@@ -0,0 +1,10 @@
+#class with functions 
+class Person:
+    def __init__ (self  , name ,age):
+        self.name = name
+        self.age = age
+name = input("Enter your Name : ")
+age = input("Enter your age : ")
+p1 = Person(name,age)
+print(p1.name)
+print(p1.age)
