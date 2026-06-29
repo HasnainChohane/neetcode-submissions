cd49c874b69fdc4fd89bc02bc85418af91f9117c
Muhammad Hasnain Maqsood <216601502+HasnainChohane@users.noreply.github.com>
2026-06-29 21:08:08 +0500

    Add files via upload

---
+++ Variables & Data Types.py
@@ -0,0 +1,23 @@
+#create variables
+Name ="Hasnain"
+Age = 21
+Height = 5.7
+Weight = 53 
+enrolled= True
+
+print("\n--- Student Profile ---")
+
+print(f"\nName: {Name}")
+print(f"Data Type: {type(Name)}")
+
+print(f"\nAge: {Age}")
+print(f"Data Type: {type(Age)}")
+
+print(f"\nHeight: {Height}m")
+print(f"Data Type: {type(Height)}")
+
+print(f"\nWeight: {Weight}kg")
+print(f"Data Type: {type(Weight)}")
+
+print(f"\nIs Enrolled: {enrolled}")
+print(f"Data Type: {type(enrolled)}")
