c85ec3ab64dcdadfb93e9261572c022affacbbb7
Muhammad Hasnain Maqsood <216601502+HasnainChohane@users.noreply.github.com>
2026-07-01 08:18:25 +0500

    Add files via upload

---
+++ compare_ops.py
@@ -0,0 +1,12 @@
+# compare_ops.py
+
+a = 10
+b = 5
+
+print("a =", a, "b =", b)
+print("a > b :", a > b)   
+print("a < b :", a < b)   
+print("a == b:", a == b) 
+print("a != b:", a != b) 
+print("a >= b:", a >= b)  
+print("a <= b:", a <= b)  
